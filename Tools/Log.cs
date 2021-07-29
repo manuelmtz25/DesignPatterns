23510d0582948aab72e5d0081ab4b57d7a616921
Manuel <manuelmtz_25@hotmail.com>
2021-07-29 10:23:51 -0500

    Add Log class and project reference

---
+++ Tools/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Tools
+{
+    public sealed class Log
+    {
+        private static readonly Log _instance = new Log();
+        private string _path = "log.txt";
+
+        public static Log Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        //Haciedo el constructor privado, se evitará crear más objetos de esta clase
+        private Log() { }
+
+        public void Save(string message)
+        {
+            File.AppendAllText(_path, message + Environment.NewLine);
+        }
+    }
+}
