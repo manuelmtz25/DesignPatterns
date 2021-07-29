4e9530f243b5cadfa8790ccc42e318cad2e35df4
Manuel <manuelmtz_25@hotmail.com>
2021-07-29 11:23:44 -0500

    Truco cuando se trabaja con hilos: lock()

--- Tools/Log.cs
+++ Tools/Log.cs
@@ -7,12 +7,22 @@ namespace Tools
     {
         private static Log _instance = null;
         private string _path;
+        private static object _protect= new object();
 
         public static Log GetInstance(string path)
         {
-            if (_instance==null)
+            //Truco cuando se trabaja con hilos.
+            //Puede pasar que dos hilos entren al mismo tiempo en este método GetInstance()
+            //los dos hilos, al acceder al método, podrían pensar que la instancia es nula
+            //y podrían crear dos instancias de forma simultanea.
+            //Mientras un hilo esté tranajando con el atributo _protect, lock() lo protegerá para que nadie más trabaje con el.
+            //Lo que va a suceder es que, mientras un hilo esté trabajando con _protect, el otro hilo va a estar en espera.
+            lock (_protect)
             {
-                _instance=new Log(path);
+                if (_instance == null)
+                {
+                    _instance = new Log(path);
+                }
             }
 
             return _instance;
